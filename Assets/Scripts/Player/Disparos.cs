1bbf658db92f8cd8242d304b55c068876edfc568
KevhSMX <k.campi@sapalomera.cat>
2026-04-22 15:51:23 +0200

    Update Disparos.cs

--- Assets/Scripts/Player/Disparos.cs
+++ Assets/Scripts/Player/Disparos.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class Disparos : MonoBehaviour
 {
-    public GameObject bolaPrefab;
+    public GameObject bolaPrefab; 
     public float velocidadBala = 15f;
     public float delayDisparo = 0.7f;
-    private float tiempoSiguiente;
+    
+    private float tiempoSiguiente; //Cooldown
 
     void Update()
     {
-        if (Time.time < tiempoSiguiente) return;
+        if (Time.time < tiempoSiguiente) return; //Mirem si el temps entre bala i bala a pasat o no.
 
+// Segons la fletxa que presionem dispara a una o un altre direcció.
         if (Input.GetKey(KeyCode.UpArrow))    Disparar(Vector2.up);
         else if (Input.GetKey(KeyCode.DownArrow))  Disparar(Vector2.down);
         else if (Input.GetKey(KeyCode.LeftArrow))  Disparar(Vector2.left);
@@ -19,16 +21,18 @@ public class Disparos : MonoBehaviour
 
     void Disparar(Vector2 direccion)
     {
-        tiempoSiguiente = Time.time + delayDisparo;
-        GameObject bola = Instantiate(bolaPrefab, transform.position, Quaternion.identity);
+        tiempoSiguiente = Time.time + delayDisparo; //Actualitzem el temps.
+        GameObject bola = Instantiate(bolaPrefab, transform.position, Quaternion.identity); //Instlanciem la bala
         
-        Rigidbody2D rb = bola.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb = bola.GetComponent<Rigidbody2D>(); //Accedim al motor de fisicas.
+        
         if (rb != null)
         {
+            //Li donem veolidad lineal a la direcció anteriorment recollida
             rb.linearVelocity = direccion * velocidadBala;
         }
 
-        Destroy(bola, 2f);
+        Destroy(bola, 2f); // La bala es destrueix si han pasat 2s
     }
 }
 
