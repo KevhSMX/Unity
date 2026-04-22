81cc74b255d4edbf7b6b9f225501440eb01da6fb
KevhSMX <k.campi@sapalomera.cat>
2026-04-22 15:37:48 +0200

    Update EnemyFall.cs

--- Assets/Scripts/Enemics/EnemyFall.cs
+++ Assets/Scripts/Enemics/EnemyFall.cs
@@ -9,17 +9,17 @@ public class EnemyFall : MonoBehaviour
 
     void Start()
     {
-        if (player == null) player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (player == null) player = GameObject.FindGameObjectWithTag("Player").transform; //Accedeix a la pos del jugador
 
-        InvokeRepeating("SpawnAbove", 0.5f, spawnRate);
+        InvokeRepeating("SpawnAbove", 0.5f, spawnRate); //Crida a la funció SpawnAbove i comença 
     }
 
     void SpawnAbove()
     {
-        if (player != null)
+        if (player != null) //Mira si el jugador es o no
         {
-            Vector3 spawnPos = new Vector3(player.position.x, player.position.y + heightOffset, 0);
-            Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+            Vector3 spawnPos = new Vector3(player.position.x, player.position.y + heightOffset, 0); //Busca la pos del jugador
+            Instantiate(enemyPrefab, spawnPos, Quaternion.identity); //Instancia el prefab del enemic
         }
     }
 }
