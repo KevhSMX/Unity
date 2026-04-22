a62d2084d3c7a0a3048729d3fe3dc238bf511eb9
KevhSMX <k.campi@sapalomera.cat>
2026-04-22 15:45:02 +0200

    Update WinTrigger.cs

--- Assets/Scripts/Map - Game/WinTrigger.cs
+++ Assets/Scripts/Map - Game/WinTrigger.cs
@@ -7,7 +7,7 @@ public class WinTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) //Si toquem aquest objecte cambiem d'escena a la de victoria.
         {
             SceneManager.LoadScene(nombreEscena);
         }
